08251d0e9f25eb3921c9452b6b4429f25bafccea
Feloriya <feloriya@gmail.com>
2025-05-08 08:49:59 +0330

    Auto-sync 2025-05-08 08:49

--- 01-Core-Concepts/Pointers/Pointer-Swap.c
+++ 01-Core-Concepts/Pointers/Pointer-Swap.c
@@ -1,7 +1,20 @@
 ﻿#include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    printf("bye Worlde azizam!\n");
-    return EXIT_SUCCESS;
+void swap(int* num1, int* num2);
+int main(void) {
+    int a;
+    int b;
+    printf("Enter Number 1 : ");
+    scanf("%d", &a);
+    printf("Enter Number 2 : ");
+    scanf("%d", &b);
+    swap(&a, &b);
+    printf("After swap Number 1 is : %d and Number 2 is : %d" , a,b);
+    return 0;
+}
+void swap(int* num1, int* num2){
+    int temp;
+    temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
 }
