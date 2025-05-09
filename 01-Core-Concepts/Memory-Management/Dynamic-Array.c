e21b8eae5223d5bcddf1ca9a0a8b37293d4b23b4
Feloriya <feloriya@gmail.com>
2025-05-09 08:07:16 +0330

    Auto-sync 2025-05-09 08:07

---
+++ 01-Core-Concepts/Memory-Management/Dynamic-Array.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int sum = 0;
+    int n;
+    do {
+    printf("Enter the number of numbers : ");
+    scanf("%d", &n);
+    } while (n < 1);
+    
+    int* userArray = (int*) calloc(n, sizeof(int));
+    
+    if (userArray == NULL) {
+        printf("Memory allocation error!\n");
+        return 1;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        printf("Enter number %d : ", i + 1);
+        scanf("%d", &userArray[i]);   
+        sum += userArray[i];
+    }
+    printf("The average is : %.2f", (sum/(float)n));
+    free(userArray);
+    return 0;
+}
